24d4b3936d0a67c65f97c0d8fdd64e89fed61964
Kanya <kanya_ykk@hotmail.com>
2025-02-20 04:06:16 +0700

    fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Blog Post API')
     .setVersion('1.0')
